ac857d568d309e4c1a02a4a271a4bfb7f52c5c67
Павел <egevtech@gmail.com>
2025-12-23 03:27:03 +0300

    Добавил структуры для десереализации

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use toml::*;
+
+#[derive(Debug, Deserialize)]
+struct Project {
+    name: String,
+    version: String,
+
+    variables: HashMap<String, String>,
+    executables: HasMap<String, Executable>
+}
+
+#[derive(Debug, Deserialize)]
+struct Executable {
+    name: String,
+
+    compiler: String,
+    linker: String,
+
+    compile_options: Vec<String>,
+    link_options: Vec<String>,
+    libraries: Vec<String>,
+
+    sources: Vec<String>
+}
+
 fn main() {
-    println!("Hello, world!");
 }
