01cdcc1d9008f6420bb131ec4b2062018b3109df
Павел <egevtech@gmail.com>
2025-12-27 11:10:36 +0300

    Добавил модули с ошибками

--- src/packer/errors.rs
+++ src/packer/errors.rs
@@ -1,5 +1,17 @@
+use std::fmt;
+
 pub enum PackerError {
     TomlError(toml::ser::Error),
     IOError(std::io::Error),
     InternalError(String),
+}
+
+impl fmt::Display for PackerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PackerError::TomlError(e) => write!(f, "Toml failed: {}", e),
+            PackerError::IOError(e) => write!(f, "IO failed: {}", e),
+            PackerError::InternalError(e) => write!(f, "Internal error: {}", e),
+        }
+    }
 }
--- src/util/error.rs
+++ src/util/error.rs
@@ -1,14 +1,16 @@
 use std::fmt;
 pub enum ArgParseError {
     ParseError(String),
-    IdentityPanic(String)
+    IdentityPanic(String),
+    ProjectPanic(String),
 }
 
 impl fmt::Display for ArgParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             ArgParseError::ParseError(msg) => write!(f, "Parse error: {}", msg),
-            ArgParseError::IdentityPanic(msg) => write!(f, "Identity panic: {}", msg)
+            ArgParseError::IdentityPanic(msg) => write!(f, "Identity panic: {}", msg),
+            ArgParseError::ProjectPanic(msg) => write!(f, "Project panic: {}", msg),
         }
     }
 }
