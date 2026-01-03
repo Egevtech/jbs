89f33f7714105950509f7b8e420cd81b15d49e1e
Egevtech <egevtech@gmail.com>
2026-01-03 06:28:16 +0300

    Change error message

--- src/derive.rs
+++ src/derive.rs
@@ -32,7 +32,7 @@ impl Value {
         match self {
             Value::String(s) => *target = s.to_string(),
             Value::VString(_) => {
-                eprintln!("Cannot append Vector to String");
+                eprintln!("Cannot insert Vector to String");
                 std::process::exit(1);
             }
         }
