9a8b5170ce11f00abdbe4979bfd5a2df3e6a85e0
Egevtech <egevtech@gmail.com>
2026-01-03 12:38:03 +0300

    Удалил старые модули парсинга аргументов

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@ mod derive;
 mod operator;
 mod packer;
 mod project;
-mod util;
 
 use crate::operator::builder::*;
 use crate::operator::configurator::configure_project;
 use crate::packer::unpack_executable::*;
 use project::*;
-use util::{parse_commands::*, *};
 
 fn main() {
     let mut project: Project;
@@ -34,39 +32,7 @@ fn main() {
         }
     };
 
-    let command: Command =
-        match parse_commands(std::env::args().collect::<Vec<String>>(), &mut project) {
-            Ok(command) => command,
-            Err(err) => {
-                println!("Argument parse error: {}", err);
-                return;
-            }
-        };
 
-    match command.command {
-        CommandType::Clean => {
-            if let Some(err) = clean() {
-                eprintln!("Failed to clean: {err}");
-            }
-        }
-        CommandType::Configure => configure_project(&project),
-        CommandType::None => println!("Nothing to do"),
-        CommandType::Build => {
-            build_project(&project);
-            drop(project);
-            std::process::exit(0);
-        }
-        CommandType::Run => {
-            build_project(&project);
-            println!("Running executable...\n");
-
-            let _ =
-                std::process::Command::new(format!("build/{}", project.default_target.unwrap()))
-                    .spawn()
-                    .expect("Cant spawn process");
-            std::process::exit(0);
-        }
-    }
 }
 
 fn build_project(project: &Project) {
